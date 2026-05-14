14d284466d00dc3af73c7c5414468611185b4656
Kevin Cuadros <kev040798@gmail.com>
2026-05-13 22:46:26 -0400

    Set connection to db

---
+++ backend/app/database.py
@@ -0,0 +1,26 @@
+from sqlalchemy.ext.asyncio import create_async_engione, AsyncSession
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_async_engione(DATABASE_URL, echo=True)
+
+AsyncSessionLocal = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
+
+class Base(DeclarativeBase):
+    pass
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+        finally:
+            await session.close()
