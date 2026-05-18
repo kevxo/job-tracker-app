7ce1a9b29b31a55ff7bddf98eb223ba47518a574
Kevin Cuadros <kev040798@gmail.com>
2026-05-17 21:08:16 -0400

    Fix db connection

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,13 +1,18 @@
-from sqlalchemy.ext.asyncio import create_async_engione, AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from dotenv import load_dotenv
 import os
+import ssl
 
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_async_engione(DATABASE_URL, echo=True)
+ssl_context = ssl.create_default_context()
+ssl_context.check_hostname = False
+ssl_context.verify_mode = ssl.CERT_NONE
+
+engine = create_async_engine(DATABASE_URL, echo=True, connect_args={"ssl": ssl_context})
 
 AsyncSessionLocal = sessionmaker(
     bind=engine,
